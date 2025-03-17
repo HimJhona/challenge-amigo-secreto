62394199fc47a10be377e0b51973fae3b3b79ee2
Jhonatan Meriño <jhonatan.merinom1@gmail.com>
2025-03-17 15:15:49 -0500

    Hice algunas modificaciones

---
+++ app.js
@@ -0,0 +1,38 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre valido.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let amigo of amigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Por favor, agrega nombres antes de sortear.");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    
+    document.getElementById("resultado").innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
